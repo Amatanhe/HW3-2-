3277cb76b3e95d2bf48f4aa529f9e2f9fb680eb2
Tatiana Tcyganova <tatiana.kv@gmail.com>
2023-07-07 21:24:40 +0300

    New excersise 19

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 21
+
+// Console.Write("Введите координату по оси оХ для третьей точки: ");
+// int x3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оY для третьей точки: ");
+// int y3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оZ для третьей точки: ");
+// int z3 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату по оси оX для второй точки: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оY для второй точки: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оZ для второй точки: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату по оси оX для первой точки: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оY для первой точки: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оZ для первой точки: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Math.Round(Math.Sqrt(Math.Pow(x3 - x2 - x1, 2) + Math.Pow(y3 - y2 - y1, 2) + Math.Pow(z3 - z2 - z1, 2)), 2);
+// Console.WriteLine($"Расстояние: {d}");
+
+
+
+// Задача 23
+// int start = 1;
+// Console.Write("Введите число: ");
+// int end = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= end; i++)
+// {
+// Console.Write(Math.Pow(i, 3) + " ");
+// }
+
+
+
+// Задача 19
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// {
+// if(number >= 10000)
+// {
+// int Digit1 = number / 10000;
+// int remainder1 = number % 10;
+// if(Digit1 == remainder1)
+// {
+// number = number / 10;
+// int Digit2 = number / 100;
+// int remainder2 = number % 10; if(Digit2 == remainder2) Console.WriteLine("Да");
+// }
+// else
+// Console.WriteLine("Нет");
+// }
+// else
+// Console.WriteLine("Некорректное число!");
+
